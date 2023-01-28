9642b75509fd8ab37312f309ce53e7ca6fdddef7
ShadePrima <aleksandrag1984@gmail.com>
2023-01-28 17:55:20 +0200

    header breeds

--- src/components/RightBreeds/RightBreeds.tsx
+++ src/components/RightBreeds/RightBreeds.tsx
@@ -7,6 +7,8 @@ import TitleSection from '../TitleSection/TitleSection';
 
 import styles from './RightBreeds.module.scss';
 import arrowButtom from '../../assets/icons/arrow-gray-bottom.png';
+import sortTop from '../../assets/icons/ab-top.png';
+import sortBottom from '../../assets/icons/ab-bottom.png';
 
 const RightBreeds: React.FC = () => {
   return (
@@ -31,6 +33,24 @@ const RightBreeds: React.FC = () => {
             </select>
             <img src={arrowButtom} alt='Arrow buttom' />
           </div>
+
+          <div className={styles.limit}>
+            <select name='limit' id='limit'>
+              <option value='1'>Limit: 5</option>
+              <option value='2'>Limit: 10</option>
+              <option value='3'>Limit: 15</option>
+              <option value='4'>Limit: 20</option>
+            </select>
+            <img src={arrowButtom} alt='Arrow buttom' />
+          </div>
+
+          <button className={styles.sortBtn}>
+            <img src={sortTop} alt='Sort top' />
+          </button>
+
+          <button className={styles.sortBtn}>
+            <img src={sortBottom} alt='Sort bottom' />
+          </button>
         </section>
       </main>
     </div>
